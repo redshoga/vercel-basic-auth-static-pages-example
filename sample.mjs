a4d4681c7d8ed35b4e8e9f641ead9693c7ec3a44
redshoga <yui.ichioka.contact@gmail.com>
2025-02-27 17:28:16 +0900

    最低限の構成に変更

--- sample.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zx
-
-await $`cat package.json | grep name`
-
-const result = await $`curl https://jsonplaceholder.typicode.com/todos/1`
-
-console.log(JSON.stringify(result, null, 2))
